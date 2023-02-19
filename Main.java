7bab7aaf6efa87d4c590b868d28876c3167257ab
Nikolas Tsalikis <124907709+NikolasTsalikis@users.noreply.github.com>
2023-02-19 00:31:23 +0000

    Text :3

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello world! This assignment took eons!!");
   }
-  //Is it? Just send this to Github?
 }
